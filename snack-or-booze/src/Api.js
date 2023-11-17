822467158e5609f4feb5d5b64b4b137dca8ea492
Tyson <trantyson@icloud.com>
2023-11-16 18:13:46 -0800

    finish exercise, problem with my machine, to finish snack/booze

--- snack-or-booze/src/Api.js
+++ snack-or-booze/src/Api.js
@@ -10,12 +10,10 @@ const BASE_API_URL = "http://localhost:5000";
 */
 
 class SnackOrBoozeApi {
-
   static async getSnacks() {
     const result = await axios.get(`${BASE_API_URL}/snacks`);
     return result.data;
   }
-
 }
 
 export default SnackOrBoozeApi;
